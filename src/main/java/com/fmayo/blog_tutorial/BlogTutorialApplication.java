50785a2caba53180350a52b00fb99c2736f313ae
fmayo96 <francomayo96@gmail.com>
2025-03-02 20:13:06 +0100

    create repo

---
+++ src/main/java/com/fmayo/blog_tutorial/BlogTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.fmayo.blog_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmayo/blog_tutorial/BlogTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmayo.blog_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
